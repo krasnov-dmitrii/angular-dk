43cf444711795a4b970e95158cbd3df5dfb4d261
krasnov-dmitrii <d.krasnov@intabia.ru>
2019-07-25 22:53:28 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-dk
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-dk)
---
+++ src/app/product-alerts/product-alerts.component.ts
@@ -0,0 +1,14 @@
+import { Component, EventEmitter } from '@angular/core';
+import { Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-product-alerts',
+  templateUrl: './product-alerts.component.html',
+  styleUrls: ['./product-alerts.component.css']
+})
+export class ProductAlertsComponent {
+
+  @Input() product;
+  @Output() notify = new EventEmitter();
+
+}
